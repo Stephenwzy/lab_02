aa31c5881c73e656ab0e6fa90b339acff2d3d6f3
Zhengyang Wang <stephen.wang.19@outlook.com>
2021-10-20 16:15:23 -0700

    1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-let icon = document.getElementsByClassName("icon")[0];
-
-icon.addEventListener('click', responsive_control);
-
-
 function responsive_control() {
     let x = document.getElementById("myTopnav");
     if (x.className === "topnav"){

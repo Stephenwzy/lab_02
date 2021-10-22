6b90bb67f4feee689bee32b1ed532547cc466b95
Zhengyang Wang <stephen.wang.19@outlook.com>
2021-10-21 21:55:59 -0700

    1

--- js/main.js
+++ js/main.js
@@ -19,10 +19,10 @@ var btn = document.getElementById("myBtn");
 function myFunction(){
     if(video.paused){
         video.play();
-        btn.innerHTML="Pause ||"
+        btn.innerHTML="Pause"
     } else {
         video.pause();
-        btn.innerHTML="Play >>";
+        btn.innerHTML="Play";
     }
 }
 
